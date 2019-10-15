c6adea4f3b946e2f950a34ce4993c609ab6ea39a
HappyCcccc <tuf99286@temple.edu>
2019-10-15 15:59:21 -0400

    Add files via upload

---
+++ GKLS_pdf_C/Software/exam.c
@@ -0,0 +1,42 @@
+/*****************************************************************************/
+/*   An example of the use of the                                            */
+/*    GKLS-Generator of Classes of ND (non-differentiable),                  */
+/*                                 D  (continuously differentiable), and     */
+/*                                 D2 (twice continuously differentiable)    */
+/*    Test Functions for Global Optimization                                 */
+/*                                                                           */
+/*   In this example, first, a class (with default parameters) of            */
+/*   100 D-type test functions is generated and the files with the function  */
+/*   information and with the functions points (in the two-dimensional case) */
+/*   are created;                                                            */
+/*   second, a file with information about gradients and Hessian of specific */
+/*   D- and D2-type test functions at a given point is provided.             */
+/*                                                                           */
+/*   Authors:                                                                */
+/*                                                                           */
+/*   M.Gaviano, D.E.Kvasov, D.Lera, and Ya.D.Sergeyev                        */
+/*                                                                           */
+/*   (C) 2002-2005                                                           */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <malloc.h>
+//#include "gkls.h"
+//#include "rnd_gen.h"
+
+
+
+int main()
+{
+
+printf("\nGKLS-Generator of Classes of ND, D, and D2 Test Functions");
+printf("\nfor Global Optimization,");
+printf("\n(C) 2002-2003, M.Gaviano, D.E.Kvasov, D.Lera, and Ya.D.Sergeyev\n");
+
+
+
+ return 0;
+
+} /* example.c */
